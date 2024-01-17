95a186bab9adcd328bdba26730590d56a4a9cf3b
kot0fey <nonch.team@gmail.com>
2024-01-17 12:56:25 +0300

    Test sumUp added. pom.xml cleaned

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,72 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = new int[300];
+        for (int i = 1; i <301; i++){
+            array[i-1] = i;
+        }
+        String s = ser(array);
+        System.out.println(s);
+        int[] newArray = deser(s);
+        for (int a : newArray){
+            System.out.println(a);
+        }
+    }
+
+
+    public static String ser(int[] array){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int num : array){
+            if (num ==0){
+                break;
+            }
+            stringBuilder
+                    .append(toBase18(num));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static int[] deser(String string){
+        String[] strNums = string.split("(?<=\\G.{2})");
+        int [] nums = new int[strNums.length];
+        for(int i = 0; i < strNums.length; i++){
+            nums[i] = fromBase18(strNums[i]);
+        }
+        return nums;
+    }
+
+
+    public static String toBase18(int n) {
+        StringBuilder result = new StringBuilder();
+        while (n > 0) {
+            int digit = n % 18;
+            if (digit < 10) {
+                result.append(digit);
+            } else {
+                result.append((char) ('A' + digit - 10));
+            }
+            n /= 18;
+        }
+        if (result.length() ==1){
+            result.append(0);
+        }
+        return result.reverse().toString();
+    }
+
+
+
+    public static int fromBase18(String n) {
+        int result = 0;
+        for (int i = n.length() - 1; i >= 0; i--) {
+            char digit = n.charAt(i);
+            if (digit >= '0' && digit <= '9') {
+                result += (digit - '0') * Math.pow(18, n.length() - 1 - i);
+            } else {
+                result += (digit - 'A' + 10) * Math.pow(18, n.length() - 1 - i);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,121 @@
-public class Test {
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import java.util.Random;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MainTest {
+    private final Random random = new Random();
+    private static Integer numberOfTests = 0;
+    private static Float sumContraction = 0F;
+
+    private void test(int[] array) {
+        String ser = Main.ser(array);
+
+        float startBytes = (float) (array.length * 4);
+        float endBytes = (float) (ser.length() / 2);
+        float contraction = ((startBytes - endBytes) / startBytes) * 100;
+
+        System.out.println("Исходная строка: " + arrayToString(array));
+        System.out.println("Сжатая строка: " + ser);
+        System.out.println("Коэффициент сжатия: " + contraction);
+        System.out.println("-----------------------------------------------------------------------------");
+
+        numberOfTests++;
+        sumContraction+=contraction;
+        assertEquals(arrayToString(array), arrayToString(Main.deser(ser)));
+    }
+
+    private String arrayToString(int[] array) {
+        StringBuilder arrayString = new StringBuilder();
+        for (int num : array) {
+            arrayString
+                    .append(num)
+                    .append(",");
+        }
+        arrayString.deleteCharAt(arrayString.length() - 1);
+        return arrayString.toString();
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        System.out.println("Средний коэффициент сжатия: " + sumContraction/numberOfTests);
+    }
+
+    //Простейшие короткие
     @Test
-    public void testEmptyArray() {
-        int[] array = new int[0];
-        assertEquals("", Ser.ser(array));
+    @Order(1)
+    public void simple() {
+        System.out.println("Простейшие короткие");
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        test(array);
+    }
+
+    //Случайные
+    @Test
+    @Order(2)
+    public void random50() {
+        System.out.println("Случайные 50");
+        int[] array = new int[50];
+        for (int i = 0; i < 50; i++) {
+            array[i] = random.nextInt(1, 301);
+        }
+        test(array);
     }
 
     @Test
-    public void testOneElementArray() {
-        int[] array = new int[]{1};
-        assertEquals("1", Ser.ser(array));
+    @Order(3)
+    public void random100() {
+        System.out.println("Случайные 100");
+        int[] array = new int[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = random.nextInt(1, 301);
+        }
+        test(array);
     }
 
     @Test
-    public void testTwoElementsArray() {
-        int[] array = new int[]{1, 2};
-        assertEquals("1.2", Ser.ser(array));
+    @Order(4)
+    public void random500() {
+        System.out.println("Случайные 500");
+        int[] array = new int[500];
+        for (int i = 0; i < 500; i++) {
+            array[i] = random.nextInt(1, 301);
+        }
+        test(array);
     }
+
+    @Test
+    @Order(5)
+    public void random1000() {
+        System.out.println("Случайные 1000");
+        int[] array = new int[1000];
+        for (int i = 0; i < 1000; i++) {
+            array[i] = random.nextInt(1, 301);
+        }
+        test(array);
+    }
+
+    //Граничные
+    @Test
+    @Order(6)
+    public void edge() {
+        System.out.println("Граничные");
+        int[] array = new int[900];
+        for (int i = 0; i < 300; i++) {
+            array[i] = i + 1;
+        }
+        for (int i = 300; i < 600; i++) {
+            array[i] = i + 1 - 300;
+        }
+        for (int i = 600; i < 900; i++) {
+            array[i] = i + 1 - 600;
+        }
+        test(array);
+    }
+
 }
